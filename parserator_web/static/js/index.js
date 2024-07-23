111f498b2a8528ac73b4dcbb7a18c192e5c8c847
ckrowe22 <100814945+ckrowe22@users.noreply.github.com>
2024-07-23 13:39:15 -0400

    Update index.js

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,40 @@
-/* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
+
+   document.getElementById("submit").addEventListener("click", function() {
+      var address = document.getElementById("address").value;
+      fetch('/api/parse/?address=' + encodeURIComponent(address))
+        .then(response => response.json())
+        .then(data => displayResults(data))
+        .catch(error => console.error('Error:', error));
+  });
+
+   function displayResults(data) {
+      document.getElementById("parse-type").innerHTML = data.address_type;
+      document.getElementById("address-results").style.display="block";
+      
+      var addressComponents = data.address_components;
+      var tableBody = document.getElementById("outputTable").getElementsByTagName('tbody')[0];
+      tableBody.innerHTML = '';
+      
+      var addresshtmlhead = `<thead>
+        <tr>
+          <th>Address part</th>
+          <th>Tag</th>
+        </tr>`
+      
+      var addressHtmlBody = '';
+      for (var key in addressComponents) {
+          if (addressComponents.hasOwnProperty(key)) {
+              addressHtmlBody += '<tr>' +
+                  '<td>' + key + '</td>' +
+                  '<td>' + addressComponents[key] + '</td>' +
+                  '</tr>';
+          }
+      }
+      
+      var addresshtmlfooter = 
+      `</thead>
+      <tbody>
+      </tbody>`;
+      
+      document.getElementById("outputTable").innerHTML = addresshtmlhead + addressHtmlBody + addresshtmlfooter;
+  }
